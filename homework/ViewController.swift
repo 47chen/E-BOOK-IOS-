9eb98276a285c2b6d9a1fd33ebb683491364c2d4
47chen <44563581+47chen@users.noreply.github.com>
2018-12-05 05:20:32 +0800

    Add files via upload

---
+++ homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework
+//
+//  Created by User06 on 2018/9/27.
+//  Copyright © 2018年 fishhChia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
